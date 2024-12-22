8ab27d723e6ca61e32b41dba500f05b8aa0329d0
Nikhil Arora <nikhilarora@adobe.com>
2024-12-23 01:27:19 +0530

    Text and text properties
    vertical arrangement properties

--- app/src/main/java/com/example/layouts/MainActivity.kt
+++ app/src/main/java/com/example/layouts/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -20,6 +21,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,10 +36,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             LayoutsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+                    MyLayout(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+//                    MyAlignments(
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
                 }
             }
         }
@@ -43,12 +54,73 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun MyAlignments(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceAround,
+
+    ) {
+        Text(
+            text = "Hello",
+            color = Color.White,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.Center,
+            modifier = modifier
+                .background(color = Color.Red)
+                .width(100.dp)
+        )
+        Text(
+            text = "Android",
+            color = Color.White,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.Center,
+            modifier = modifier
+                .background(color = Color.Red)
+                .width(100.dp)
+        )
+        Text(
+            text = "Kotlin",
+            color = Color.White,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.Center,
+            modifier = modifier
+                .background(color = Color.Red)
+                .width(100.dp)
+        )
+
+    }
+
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun MyLayout(name: String, modifier: Modifier = Modifier) {
     Column {
-        Box(modifier = Modifier.background(Color.Red).height(200.dp).width(300.dp)) {
+        Box(
+            modifier = Modifier
+                .background(Color.Red)
+                .height(200.dp)
+                .width(300.dp)
+        ) {
             Row()
             {
-                Box(modifier = Modifier.background(Color.Blue).size(50.dp)){}
+                Box(
+                    modifier = Modifier
+                        .background(Color.Blue)
+                        .size(50.dp)
+                ) {}
                 Column {
                     Text(
                         text = "Hello",
@@ -72,6 +144,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     LayoutsTheme {
-        Greeting("Android")
+//        Greeting("Android")
+//        MyLayout("Android")
+        MyAlignments()
     }
 }
